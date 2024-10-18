d4e5c457e874fc0f5b59b94db05144a922f6a9b0
Luan Felipe Antunes <luanfelipe12.antunes@gmail.com>
2024-10-17 22:27:17 -0300

    Add images

--- solarman-backend/resources/views/emails/client_created.blade.php
+++ solarman-backend/resources/views/emails/client_created.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Novo orçamento gerado</title>
-    <link rel="stylesheet" href="https://raw.githubusercontent.com/jiladev/solarman/refs/heads/main/solarman-backend/public/css/style.css?token=GHSAT0AAAAAACYMDKKBWCAPWBPIJPNUPG2OZYRW4OQ">
+    <link rel="stylesheet" href="https://github.com/jiladev/solarman/blob/main/solarman-backend/public/css/style.css">
 
 </head>
 
@@ -13,7 +13,7 @@
     <div class="container">
         <header class="header">
             <div class="img-header">
-                <img src="https://raw.githubusercontent.com/jiladev/solarman/refs/heads/main/solarman-backend/public/img/logo-horiz-solarman-1.svg?token=GHSAT0AAAAAACYMDKKAYFMLT3H3W2MVRQ7SZYRW3IQ" alt="Logo Solarman">
+                <img src="https://raw.githubusercontent.com/jiladev/solarman/refs/heads/main/solarman-backend/public/img/logo-horiz-solarman-1.png?token=GHSAT0AAAAAACYMDKKAYFMLT3H3W2MVRQ7SZYRW3IQ" alt="Logo Solarman">
             </div>
             <h1> Tem <span>cliente novo</span> chegando aí!</h1>
             <hr class="line-orange">
@@ -36,7 +36,7 @@
     </div>
 
     <footer class="footer">
-        <img src="https://raw.githubusercontent.com/jiladev/solarman/refs/heads/main/solarman-backend/public/img/logo-horiz-solarman-branca-1.svg?token=GHSAT0AAAAAACYMDKKA3PET4GSL3URNNG5UZYRW3WA" alt="Logo footer">
+        <img src="https://raw.githubusercontent.com/jiladev/solarman/refs/heads/main/solarman-backend/public/img/logo-horiz-solarman-branca-1.png?token=GHSAT0AAAAAACYMDKKA3PET4GSL3URNNG5UZYRW3WA" alt="Logo footer">
     </footer>
 
 </body>
